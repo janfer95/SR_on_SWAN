f8b84cb76f9c4853d7318b356bcf203cc3d0dba4
Kuhn Jannik <jannik.kuhn@univ-pau.fr>
2024-04-09 16:50:55 +0200

    Reformat utilities file and add docstrings
    Adds mainly a lot of documentation to make the functions clearer
    and changed some argument names for clarity.

--- NotebooksAndScripts/utilities.py
+++ NotebooksAndScripts/utilities.py
@@ -1,16 +1,31 @@
-"""This module contains the subpixel layer class and
-various other often-used functions to keep the
-training scripts and jupyter notebooks tidy."""
+"""Various utility functions that are used repeatedly throughout the
+jupyter notebooks and model scripts, such as for loading data or writing
+and working with summary files.
+"""
+
 import numpy as np
-import glob
 
 from datetime import date, timedelta
 from pathlib import Path
+from typing import Callable, Iterable, Iterator, Literal, Union
 
 
-# Utility Functions
-def uniquify(paths):
-    """If model name already exists, add a subscript."""
+PathLike = Union[str, Path]
+
+
+def uniquify(paths: list[Path]) -> list[Path]:
+    """Adds a counter to the end of the filename if it already exists.
+
+    Parameters
+    ----------
+    paths : list[Path]
+        List of paths to files that need to be uniquified
+
+    Returns
+    -------
+    list[Path]
+        List of paths to files with the added counter
+    """
     paths_out = []
     for path in paths:
         path = Path(path)
@@ -25,62 +40,127 @@ def uniquify(paths):
     return paths_out
 
 
-def create_dategenerator(time_step=1, time_step_type="hrs"):
-    """Define help function to create a generator that outputs a
-    datetime object for the given time range."""
+def create_dategenerator(
+    time_step: int = 1,
+    time_step_type: Literal["hrs", "min"] = "hrs"
+) -> Callable[[date, date], Iterator[date]]:
+    """Create a generator that yields a date range with a given time step.
+
+    Parameters
+    ----------
+    time_step : int, optional
+        Time step in hours or minutes, by default 1
+    time_step_type : Literal["hrs", "min"], optional
+        Type of time step, by default "hrs"
+
+    Returns
+    -------
+    Callable[[date, date], Iterator[date]]
+        Generator that yields a date range with the given time step
+    """
     if time_step_type == "hrs":
-        const = int(24/time_step)
+        const = int(24 / time_step)
 
         def daterange(start_date, end_date):
-            for n in range(const*int((end_date - start_date).days)):
-                yield start_date + n*timedelta(hours=time_step)
+            for n in range(const * int((end_date - start_date).days)):
+                yield start_date + n * timedelta(hours=time_step)
 
-    else:
-        const = int(60/time_step * 24)
+    elif time_step_type == "min":
+        const = int(60 / time_step * 24)
 
         def daterange(start_date, end_date):
-            for n in range(const*int((end_date - start_date).days)):
-                yield start_date + n*timedelta(minutes=time_step)
+            for n in range(const * int((end_date - start_date).days)):
+                yield start_date + n * timedelta(minutes=time_step)
 
     return daterange
 
 
-def get_dict(filename):
-    """
-    Get the summary file as a dictionary.
+def get_dict(
+    filename: PathLike
+) -> dict[str, str]:
+    """Get the summary file as a dictionary.
+
+    Parameters
+    ----------
+    filename : PathLike
+        Path to the summary file
+
+    Returns
+    -------
+    dict[str, str]
+        Dictionary with the summary file contents
     """
     with open(filename) as f:
         # Get everything into a dict
-        d = dict(line.split(': ', 1) for line in f)
+        d = dict(line.split(": ", 1) for line in f)
 
     return d
 
 
-def get_file_list(dr, var, dir_prefix="Models",
-                  file_prefix="Summary_{}*"):
+def get_file_list(
+    dr: PathLike,
+    var: str,
+    root_dir: PathLike = "Models",
+    file_prefix: str = "Summary_{}*"
+) -> list[Path]:
+    """Get a list of summary files, given a model directory and a variable.
+
+    Parameters
+    ----------
+    dr : PathLike
+        Model directory
+    var : str
+        Variable name.
+    root_dir : PathLike, optional
+        Root directory of the models, by default "Models"
+    file_prefix : str, optional
+        File prefix for the summary files, by default "Summary_{}*"
+
+    Returns
+    -------
+    list[Path]
+        List of summary files in the given directory
     """
-    Get a list of summary files, given a model directory and a variable.
-    """
-    
-    fdir = Path(dir_prefix) / dr
+
+    fdir = Path(root_dir) / dr
 
     return fdir.glob(file_prefix.format(var))
 
 
-def extract_time_and_eval(dr, var, **kwargs):
+def extract_time_and_eval(
+    dr: PathLike,
+    var: str,
+    root_dir: PathLike = "Models",
+    file_prefix: str = "Summary_{}*"
+) -> tuple[list[float], list[float]]:
+    """Extract the run time and the final evaluation loss of the model
+
+    Note that the time is in hours.
+
+    Parameters
+    ----------
+    dr : PathLike
+        Model directory
+    var : str
+        Variable name.
+    root_dir : PathLike, optional
+        Root directory of the models, by default "Models"
+    file_prefix : str, optional
+        File prefix for the summary files, by default "Summary_{}*"
+
+    Returns
+    -------
+    tuple[list[float], list[float]]
+        List of evaluation losses and run times
     """
-    Extract the run time and the final evaluation of the model,
-    given a model directory and a variable. Convert times into hours.
-    """
-    # Get the file list
-    flist = get_file_list(dr, var, **kwargs)
+    flist = get_file_list(dr, var, root_dir, file_prefix)
 
     times = []
     evals = []
 
     for filename in flist:
         d = get_dict(filename)
-        time = float(d["Time"])/3600
+        time = float(d["Time"]) / 3600
         ev = float(d["Eval"])
 
         times.append(time)
@@ -89,42 +169,100 @@ def extract_time_and_eval(dr, var, **kwargs):
     return evals, times
 
 
-def write_summary(fsum, var, t0, random_state_train,
-                  random_state_test, min_val_loss, ev,
-                  train_size=0.8, batch_size=32,
-                  patience=30):
-    """Write the summary file."""
-    summary = """Date: {}
-    Variable: {}
-    Train_Size: {}
-    Patience: {}
-    Batch_Size: {}
-    Time: {}
-    Random_State_Train: {}
-    Random_State_Test: {}
-    min_val_loss: {}
-    Eval: {}
+def write_summary(
+    fsum: PathLike,
+    var: str,
+    t0: float,
+    random_state_train: int,
+    random_state_test: int,
+    min_val_loss: float,
+    ev: float,
+    train_size: float = 0.8,
+    batch_size: int = 32,
+    patience: int = 30,
+) -> None:
+    """Write the summary file for the model.
+
+    Parameters
+    ----------
+    fsum : PathLike
+        Path to the summary file
+    var : str
+        Variable name
+    t0 : float
+        Run time of the model
+    random_state_train : int
+        Random state for the training set
+    random_state_test : int
+        Random state for the test set
+    min_val_loss : float
+        Minimum validation loss
+    ev : float
+        Evaluation / Test loss
+    train_size : float, optional
+        Fraction of data set used for training, by default 0.8
+    batch_size : int, optional
+        Batch size, by default 32
+    patience : int, optional
+        Patience for early stopping, by default 30
+
+    Returns
+    -------
+    None
+    """
+    summary = """Date: {today}
+    Variable: {var}
+    Train_Size: {train_size}
+    Patience: {patience}
+    Batch_Size: {batch_size}
+    Time: {t0}
+    Random_State_Train: {random_state_train}
+    Random_State_Test: {random_state_test}
+    min_val_loss: {min_val_loss}
+    Eval: {ev}
     """
 
     # Get the current date
     today = date.today().strftime("%Y%m%d")
 
     with open(fsum, "w") as f:
-        summary = summary.format(today, var, train_size, patience,
-                                 batch_size, t0, random_state_train,
-                                 random_state_test, min_val_loss, ev)
+        summary = summary.format(
+            today=today,
+            var=var,
+            train_size=train_size,
+            patience=patience,
+            batch_size=batch_size,
+            t0=t0,
+            random_state_train=random_state_train,
+            random_state_test=random_state_test,
+            min_val_loss=min_val_loss,
+            ev=ev,
+        )
         f.write(summary)
 
 
-def get_info_file_names(fdir, var):
-    """Return the paths to the information files."""
-    fdir = Path(fdir)
-    
-    # Check if Model Folder already exists, if not, create it
-    if not fdir.exists():
-        fdir.mkdir(parents=True)
+def get_info_file_names(fdir: PathLike, var: str) -> tuple[Path, Path, Path]:
+    """Create and return the paths to the information files.
 
-    # Get the current date for the model name
+    If necessary the filenames are made unique by appending a suffix counter.
+
+    Parameters
+    ----------
+    fdir : PathLike
+        Model directory to save the files in
+    var : str
+        Variable name
+
+    Returns
+    -------
+    tuple[Path, Path, Path]
+        Paths to the model, history, and summary files
+    """
+    fdir = Path(fdir)
+
+    fdir.mkdir(parents=True, exist_ok=True)
+
+    # Get the current date to append to the model name
     today = date.today().strftime("%Y%m%d")
 
     # Define the model names
@@ -144,10 +282,41 @@ def get_info_file_names(fdir, var):
     return fmodel, fhist, fsum
 
 
-# Data Handling
-def load_data(fnames, serial, var, grid=(10, 10), grid_out=(160, 160),
-              dim=1, convert=True):
-    """Load input and reference data."""
+def load_data(
+    fnames: tuple[str, str],
+    serial: Iterable[int],
+    var: str,
+    grid: tuple[int, int] = (10, 10),
+    grid_out: tuple[int, int] = (160, 160),
+    dim: int = 1,
+    convert: bool = False
+) -> tuple[np.ndarray, np.ndarray]:
+    """Load the data from the given filename patterns and serial numbers.
+
+    Parameters
+    ----------
+    fnames : tuple[str, str]
+        Tuple of filename patterns for the high and low resolution data
+    serial : Iterable[int]
+        Serial numbers of the data to load
+    var : str
+        Variable name
+    grid : tuple[int, int], optional
+        Grid size of the input data, by default (10, 10)
+    grid_out : tuple[int, int], optional
+        Grid size of the output data, by default (160, 160)
+    dim : int, optional
+        Number of grid dimensions. Can be useful for multiple input types,
+        such as wave height AND wave period. By default 1
+    convert : bool, optional
+        Convert directional data to a range without the 360°-0° discontinuity,
+        by default False
+
+    Returns
+    -------
+    tuple[np.ndarray, np.ndarray]
+        Tuple of low-resolution input and high-resolution output data
+    """
     fname_HR, fname_LR = fnames
     n_serial = len(serial)
     # Initialize input and labels arrays respectively
@@ -170,21 +339,47 @@ def load_data(fnames, serial, var, grid=(10, 10), grid_out=(160, 160),
     return X_tot, y_tot
 
 
-def NNUpsampling(arr, factor=16):
-    """
-    Simple implementation of nearest neighbor upsampling
-    that works on numpy arrays.
-    """
+def NNUpsampling(arr: np.ndarray, factor: int = 16) -> np.ndarray:
+    """Simple implementation of nearest neighbor upsampling in numpy.
 
+    Parameters
+    ----------
+    arr : np.ndarray
+        Array to upsample
+    factor : int, optional
+        Upsampling factor, by default 16
+
+    Returns
+    -------
+    np.ndarray
+        Upsampled array
+    """
     return arr.repeat(factor, axis=0).repeat(factor, axis=1)
 
 
-def get_data_in_box(coords, bathy, tr, bl):
-    """
-    Given the bathymetry data (and the corresponding
-    coordinates), extract only the data in the
-    rectangle defined by top right (tr) and
-    bottom left (bl) point.
+def get_data_in_box(
+    coords: np.ndarray,
+    bathy: np.ndarray,
+    tr: tuple[float, float],
+    bl: tuple[float, float],
+) -> tuple[np.ndarray, np.ndarray]:
+    """Extract the bathymetry data and coordinates within the defined box.
+
+    Parameters
+    ----------
+    coords : np.ndarray
+        Coordinates of the data
+    bathy : np.ndarray
+        Bathymetry data
+    tr : tuple[float, float]
+        Top right corner of the box
+    bl : tuple[float, float]
+        Bottom left corner of the box
+
+    Returns
+    -------
+    tuple[np.ndarray, np.ndarray]
+        Coordinates and bathymetry data within the box
     """
     # Define the conditions if coord is in box
     condition1 = coords[:, 0] > bl[0]
